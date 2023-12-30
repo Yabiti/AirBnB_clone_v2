4f660c4055d8bb8f8472f70f18f4a538e328e0ad
Yabiti <yabetseyohannes@gmail.com>
2023-12-30 11:16:27 +0300

    .

--- web_flask/4-number_route.py
+++ web_flask/4-number_route.py
@@ -3,3 +3,20 @@
 from flask import flask
 
 app = flask(__name__)
+
+app.route('/', strict_slashes=False)
+
+def HELLO_HBNB():
+    """displays HELLO_HBNB"""
+    return "HELLO_HBNB"
+
+app.route('/hbnb', strict_slashes=False)
+
+def hbnb():
+    """displays hbnb"""
+    return "hbnb"
+
+app.route('/c/<text', strict_slashes=False)
+def c_with_text(text):
+    formatted_text = text.replace('_',' ')
+    return "c ()". format.formatted_text()
