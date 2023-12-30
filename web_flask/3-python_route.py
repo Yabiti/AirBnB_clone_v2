32c0fc2c02faffa2df1ab8793b3e29589a4df510
Yabiti <yabetseyohannes@gmail.com>
2023-12-30 11:06:31 +0300

    .

--- web_flask/3-python_route.py
+++ web_flask/3-python_route.py
@@ -24,3 +24,7 @@ def c_with_text(text):
 #Define the route for /python<text>
 app.route('/python', defaults={'text: is cool'}, strict_slashes=False)
 app.route('/python<text>', strict_slashes=False)
+
+def python_with_text(text):
+    formatted_text = text.replace('_',' ')
+    return "c ()". format.formatted_text()
