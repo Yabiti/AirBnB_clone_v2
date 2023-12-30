93c4493f9bcfc0a47d2f3fa8e371e21abbdc9d99
Yabiti <yabetseyohannes@gmail.com>
2023-12-30 11:19:54 +0300

    .

--- web_flask/4-number_route.py
+++ web_flask/4-number_route.py
@@ -23,3 +23,7 @@ def c_with_text(text):
 
 app.route('/python', defaults={'text: is cool'}, strict_slashes=False)
 app.route('/python<text>', strict_slashes=False)
+
+def python_with_text(text):
+    formatted_text = text.replace('_',' ')
+    return "c ()", format.formatted_text()
